31a0c6b102493faea0ed8f9de64979f8bc275348
Joseph Book Book <josephbenjaminbookbook@gmail.com>
2024-12-18 08:23:08 -0600

    updating my route table

---
+++ resources/dev/vpc_demo/main.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_version = ">= 1.0.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
+module "vpc" {
+  source = "../../../terraform/module"  # Update this path to point to your module location
+
+  aws_region           = var.aws_region
+  vpc_cidr             = var.vpc_cidr
+  public_subnet_cidrs  = var.public_subnet_cidrs
+  private_subnet_cidrs = var.private_subnet_cidrs
+  availability_zones   = var.availability_zones
+  tags                 = var.tags
+}
